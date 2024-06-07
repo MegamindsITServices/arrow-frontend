b9bb526e63cd0f901bf58ba3f3d88f08f1115649
MegamindsITServices <megamindsresearch@gmail.com>
2024-06-08 01:54:11 +0530

    Update Eresources.js

--- src/pages/Eresources.js
+++ src/pages/Eresources.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Layout from "../components/Layout/Layout";
 const Eresources = () => {
   return (
-    <>
+    <Layout>
       <h1 className="d-flex justify-content-center align-items-middle mt-4">
         <div className="pnf">
-          <h1 className="pnf-title">503</h1>
-          <h2 className="pnf-heading">PAGE IS UNDER CONSTRUCTION.</h2>
+          <img src="images/page-maintainance.jpeg" className="w-100" />
           <Link to="/" className="pnf-btn">
             Back to Home
           </Link>
         </div>
       </h1>
-    </>
+    </Layout>
   );
 };
 
