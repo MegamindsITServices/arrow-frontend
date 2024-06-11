7951eb4735a7ca750fb508fddcaaf414c4437ec5
MegamindsITServices <megamindsresearch@gmail.com>
2024-06-11 12:58:52 +0530

    Update CreateAdmin.js

--- src/pages/Admin/CreateAdmin.js
+++ src/pages/Admin/CreateAdmin.js
@@ -11,10 +11,16 @@ const CreateAdmin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
   const [securityQuestion, setSecurityQuestion] = useState("");
   const [answer, setAnswer] = useState("");
   const [captchaValue, setCaptchaValue] = useState("");
+  const [shippingAddress, setShippingAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [locality, setLocality] = useState("");
+  const [state, setState] = useState("");
+  const [district, setDistrict] = useState("");
+  const [pincode, setPincode] = useState("");
+  const [landmark, setLandmark] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -25,6 +31,15 @@ const CreateAdmin = () => {
       return;
     }
     try {
+      const address = {
+        shippingAddress,
+        city,
+        locality,
+        state,
+        district,
+        pincode,
+        landmark,
+      };
       const res = await axios.post("/api/v1/auth/admin-register", {
         name,
         email,
@@ -55,120 +70,195 @@ const CreateAdmin = () => {
         <div className="form-container">
           {/* <h1>Register</h1> */}
           <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="form-control input-login"
-                id="exampleInputEmail1"
-                placeholder="Name"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="form-control input-login"
-                id="exampleInputEmail1"
-                placeholder="Email"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="form-control input-login"
-                id="exampleInputPassword1"
-                placeholder="Password"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <input
-                type="text"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                className="form-control input-login"
-                id="exampleInputEmail1"
-                // pattern="[7-9]{1}[0-9]{9}"
-                // title="Phone number with 7-9 and remaining 9 digit with 0-9"
-                placeholder="Phone"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <input
-                type="text"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                className="form-control input-login"
-                id="exampleInputEmail1"
-                placeholder="Address"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <select
-                value={securityQuestion}
-                onChange={(e) => setSecurityQuestion(e.target.value)}
-                className="form-control input-login"
-                id="securityQuestion"
-                required
-              >
-                <option value="">Select Security Question</option>
-                <option value="favoritePetName">Favorite Pet Name</option>
-                <option value="favoriteFood">Favorite Food</option>
-                <option value="childhoodbestfriend">
-                  your childhood best friend
-                </option>
-                {/* Add more security questions as needed */}
-              </select>
-            </div>
-            {securityQuestion === "favoriteFood" && (
-              <div className="mb-3">
-                <input
-                  type="text"
-                  value={answer}
-                  onChange={(e) => setAnswer(e.target.value)}
-                  className="form-control input-login"
-                  id="answer"
-                  placeholder="Enter Favorite Food"
-                  required
-                />
+            <div className="row w-100 form-content">
+              <div className="col-md-6">
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control input-login"
+                    id="exampleInputEmail1"
+                    placeholder="Name"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control input-login"
+                    id="exampleInputEmail1"
+                    placeholder="Email"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control input-login"
+                    id="exampleInputPassword1"
+                    placeholder="Password"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="form-control input-login"
+                    id="exampleInputEmail1"
+                    pattern="\d*"
+                    // pattern="[7-9]{1}[0-9]{9}"
+                    // title="Phone number with 7-9 and remaining 9 digit with 0-9"
+                    placeholder="Phone"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={shippingAddress}
+                    onChange={(e) => setShippingAddress(e.target.value)}
+                    className="form-control input-login"
+                    id="exampleInputEmail1"
+                    placeholder="Address"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <select
+                    value={securityQuestion}
+                    onChange={(e) => setSecurityQuestion(e.target.value)}
+                    className="form-control input-login"
+                    id="securityQuestion"
+                    required
+                  >
+                    <option value="">Select Security Question</option>
+                    <option value="favoritePetName">Favorite Pet Name</option>
+                    <option value="favoriteFood">Favorite Food</option>
+                    <option value="childhoodbestfriend">
+                      your childhood best friend
+                    </option>
+                    {/* Add more security questions as needed */}
+                  </select>
+                </div>
+                {securityQuestion === "favoriteFood" && (
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      value={answer}
+                      onChange={(e) => setAnswer(e.target.value)}
+                      className="form-control input-login"
+                      id="answer"
+                      placeholder="Enter Favorite Food"
+                      required
+                    />
+                  </div>
+                )}
+                {securityQuestion === "favoritePetName" && (
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      value={answer}
+                      onChange={(e) => setAnswer(e.target.value)}
+                      className="form-control input-login"
+                      id="answer"
+                      placeholder="Enter pet name"
+                      required
+                    />
+                  </div>
+                )}
+                {securityQuestion === "childhoodbestfriend" && (
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      value={answer}
+                      onChange={(e) => setAnswer(e.target.value)}
+                      className="form-control input-login"
+                      id="answer"
+                      placeholder="Enter your childhood best friend"
+                      required
+                    />
+                  </div>
+                )}
               </div>
-            )}
-            {securityQuestion === "favoritePetName" && (
-              <div className="mb-3">
-                <input
-                  type="text"
-                  value={answer}
-                  onChange={(e) => setAnswer(e.target.value)}
-                  className="form-control input-login"
-                  id="answer"
-                  placeholder="Enter pet name"
-                  required
-                />
+
+              <div className="col-md-6">
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={state}
+                    onChange={(e) => setState(e.target.value)}
+                    className="form-control input-login"
+                    id="exampleInputEmail1"
+                    placeholder="State"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={district}
+                    onChange={(e) => setDistrict(e.target.value)}
+                    className="form-control input-login"
+                    id="exampleInputEmail1"
+                    placeholder="District"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    className="form-control input-login"
+                    id="exampleInputEmail1"
+                    placeholder="City"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={locality}
+                    onChange={(e) => setLocality(e.target.value)}
+                    className="form-control input-login"
+                    id="exampleInputEmail1"
+                    placeholder="Locality"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={landmark}
+                    onChange={(e) => setLandmark(e.target.value)}
+                    className="form-control input-login"
+                    id="exampleInputEmail1"
+                    placeholder="Landmark"
+                    required
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    pattern="\d*"
+                    value={pincode}
+                    onChange={(e) => setPincode(e.target.value)}
+                    className="form-control input-login"
+                    id="exampleInputEmail1"
+                    placeholder="Pincode"
+                    required
+                  />
+                </div>
               </div>
-            )}
-            {securityQuestion === "childhoodbestfriend" && (
-              <div className="mb-3">
-                <input
-                  type="text"
-                  value={answer}
-                  onChange={(e) => setAnswer(e.target.value)}
-                  className="form-control input-login"
-                  id="answer"
-                  placeholder="Enter your childhood best friend"
-                  required
-                />
-              </div>
-            )}
-            {/* <div className="mb-3">
+              {/* <div className="mb-3">
               <input
                 type="text"
                 value={answer}
@@ -180,15 +270,16 @@ const CreateAdmin = () => {
                 style={{ width: "86%" }}
               />
             </div> */}
-            <ReCAPTCHA
-              sitekey="6Ldp758pAAAAAKJ-yqRCEOVuyKVUDxkYRX06dVaC"
-              onChange={onChangeCaptcha}
-              className="mb-4"
-            />
+              <ReCAPTCHA
+                sitekey="6Ldp758pAAAAAKJ-yqRCEOVuyKVUDxkYRX06dVaC"
+                onChange={onChangeCaptcha}
+                className="mb-4"
+              />
 
-            <button type="submit" className="login-button">
-              Submit
-            </button>
+              <button type="submit" className="login-button">
+                Submit
+              </button>
+            </div>
           </form>
         </div>
       </Layout>
