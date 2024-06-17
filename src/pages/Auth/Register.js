4becab4c966fb3a1e45f2819827d084c567a2eb2
MegamindsITServices <megamindsresearch@gmail.com>
2024-06-17 11:14:34 +0530

    Update Register.js

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -269,7 +269,7 @@ const Register = () => {
               />
             </div> */}
               <ReCAPTCHA
-                sitekey="6Ldp758pAAAAAKJ-yqRCEOVuyKVUDxkYRX06dVaC"
+                sitekey="6LdBiPopAAAAAByRk-w6oHFApDI5t_w7GdRRyfOO"
                 onChange={onChangeCaptcha}
                 className="mb-4"
               />
